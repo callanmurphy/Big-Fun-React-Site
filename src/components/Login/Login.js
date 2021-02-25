988de4a2a2bdff52517e50eb56f337b5d33b9f7a
callanmurphy <callanmurphy31@gmail.com>
2021-02-25 03:29:03 -0500

    createAccount and Login components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import './Login.css';
 
 class Login extends Component {
-  render() {
-    return <div>Login</div>;
+  constructor(props){
+      super(props);
+      this.state = {
+          username: '',
+          password: '',
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(e){
+      this.setState({[e.target.id]: e.target.value});
+  }
+    
+  handleSubmit(e){
+      e.preventDefault();
+      alert(this.state.username + " logged in successfully");
+      window.location.href = "/home";
+  }
+
+  render() {
+      
+    return (
+    <div class='centerText centerBox'>
+      <h1>Login</h1>
+      <form name='registerForm' onSubmit={this.handleSubmit}>
+        <input type="text" id='username' placeholder='Username' value={this.state.username} onChange={this.handleChange.bind(this)} />
+        <br/>
+        <input type="password" id='password' placeholder='Password' value={this.state.password} onChange={this.handleChange.bind(this)} />
+        <br/><br/>
+        <input type="submit" class='submitButton' value="Login"/>
+      </form>
+    </div>
+    );
+  }
+
 }
 
 export default Login;
