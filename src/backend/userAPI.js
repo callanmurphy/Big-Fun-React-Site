a96eb19268ed5d7f6bb1fea763f310b3ebc3b66e
Zakir Chaudry <zakirchaudry@gmail.com>
2021-03-01 20:46:40 -0500

    added new user properties

--- src/backend/userAPI.js
+++ src/backend/userAPI.js
@@ -1,9 +1,9 @@
 const users = [
-  {id: 0, name: 'admin', password: 'admin', online: false},
-  {id: 0, name: 'user1', password: 'user1', online: true},
-  {id: 0, name: 'user2', password: 'user2', online: false},
-  {id: 0, name: 'user3', password: 'user3', online: true},
-  {id: 0, name: 'user4', password: 'user4', online: false},
+  {id: 0, name: 'admin', password: 'admin', online: false, profilePic: 0, rivals: ['user1', 'user2', 'user3', 'user4']},
+  {id: 0, name: 'user1', password: 'user1', online: true, profilePic: 2, rivals: ['admin', 'user2', 'user4']},
+  {id: 0, name: 'user2', password: 'user2', online: false, profilePic: 2, rivals: ['admin', 'user1', 'user4']},
+  {id: 0, name: 'user3', password: 'user3', online: true, profilePic: 1, rivals: ['admin', 'user4']},
+  {id: 0, name: 'user4', password: 'user4', online: false, profilePic: 2, rivals: ['admin', 'user1', 'user2', 'user3']},
 ]
 
 export function getUser(id) {
