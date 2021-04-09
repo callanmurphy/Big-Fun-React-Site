22229225f974e8a7ab9844bf590aba6146408b89
Callan Murphy <callanmurphy31@gmail.com>
2021-04-09 18:16:13 -0400

    fixed missing game error

--- client/src/components/Games/GamePages/TypeType.js
+++ client/src/components/Games/GamePages/TypeType.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 
 class TypeType extends Component {
+  constructor(props) {
+    super(props);
+    this.containerRef = React.createRef();
+    this.ballRef = React.createRef();
+
+    this.state = {
+      score: 0,
+      life: 10,
+    }
+
+    this.dims = {
+      height: null,
+      width: null,
+    }
+  }
+  
   render() {
     return <div><h1>COMING SOON !!!!!!!!</h1></div>;
   }
--- client/src/components/Games/GamePages/index.js
+++ client/src/components/Games/GamePages/index.js
@@ -1,7 +1,6 @@
 import Pong from './Pong';
 import TypeType from './TypeType';
 import DotFollow from './DotFollow';
-import TicTacToe from './TicTacToe';
 
 
 export { Pong, TypeType, DotFollow, TicTacToe };
